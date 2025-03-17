da0b82d8f134773c39f31a1d4b2728978ac029af
Fareselawady <121154283+Fareselawady@users.noreply.github.com>
2025-03-17 14:21:22 +0200

    Update Program.cs

--- finall project/finall project/Program.cs
+++ finall project/finall project/Program.cs
@@ -1,67 +1,110 @@
-﻿using System.Xml.Linq;
+using System;
+using System.Collections.Generic;
 
-namespace finall_project
+namespace FinalProject
 {
     public delegate void TicketBookingHandler(string message);
-    public interface Iticketgooking
+
+    public interface ITicketBooking
     {
-        void Bookticket(int ticketId);
-        void Cancelticket(int ticketId);
-        void Displayavailableticket();
+        void BookTicket(int ticketId);
+        void CancelTicket(int ticketId);
+        void DisplayAvailableTickets();
     }
-    public class Ticket 
+
+    public class Ticket
     {
-        public Ticket(int id, string name, TicketType type, decimal money)
+        public Ticket(int id, string name, TicketType type, decimal price)
         {
-            this.ticketType = type;
-            this.price = money;
-            this.Eventname = name;
-            this.Bookid = id;
+            this.TicketType = type;
+            this.Price = price;
+            this.EventName = name;
+            this.BookId = id;
         }
+
         public override string ToString()
         {
-            return $"{Bookid} | {Eventname} | {ticketType} | {price}";
+            return $"{BookId} | {EventName} | {TicketType} | {Price} EGP";
         }
-        public int Bookid { get; set; }
-        public string Eventname { get; set; }
-        public TicketType ticketType { get; set; }
-        public decimal price { get; set; }
+
+        public int BookId { get; set; }
+        public string EventName { get; set; }
+        public TicketType TicketType { get; set; }
+        public decimal Price { get; set; }
     }
 
-    public class Ticketbookingmanagment : Iticketgooking
+    public class TicketBookingManagement : ITicketBooking
     {
-       public List<Ticket> list = new List<Ticket>();
-       public event TicketBookingHandler TicketBooked;
-        public void Bookticket(int ticketId)
-        {
+        private List<Ticket> tickets = new List<Ticket>();
 
+        public event TicketBookingHandler TicketBooked;
+
+        public void BookTicket(int ticketId)
+        {
+            var ticket = tickets.Find(t => t.BookId == ticketId);
+            if (ticket != null)
+            {
+                tickets.Remove(ticket);
+                TicketBooked?.Invoke($"Ticket {ticketId} booked successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Ticket not found.");
+            }
         }
 
-        public void Cancelticket(int ticketId)
+        public void CancelTicket(int ticketId)
         {
-
+            Console.WriteLine($"Ticket {ticketId} canceled.");
         }
 
-        public void Displayavailableticket()
+        public void DisplayAvailableTickets()
         {
+            if (tickets.Count == 0)
+            {
+                Console.WriteLine("No tickets available.");
+                return;
+            }
 
+            Console.WriteLine("Available Tickets:");
+            foreach (var ticket in tickets)
+            {
+                Console.WriteLine(ticket);
+            }
         }
 
+        public void AddTicket(Ticket ticket)
+        {
+            tickets.Add(ticket);
+        }
     }
+
     public enum TicketType
     {
-        Regylar,
+        Regular,
         VIP,
         Child
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            var TBM = new Ticketbookingmanagment();
-           TBM.list.Add(new Ticket(1 , "fares" , TicketType.VIP , 300.00m));
-           TBM.list.Add(new Ticket(2 , "Elawady" , TicketType.Child , 50.00m));
-            TBM.list.Add(new Ticket(3, "Mawlana", TicketType.Regylar, 100.00m));
+            var tbm = new TicketBookingManagement();
+
+            // Adding tickets
+            tbm.AddTicket(new Ticket(1, "Concert", TicketType.VIP, 300.00m));
+            tbm.AddTicket(new Ticket(2, "Movie", TicketType.Child, 50.00m));
+            tbm.AddTicket(new Ticket(3, "Theater", TicketType.Regular, 100.00m));
+
+            // Display available tickets
+            tbm.DisplayAvailableTickets();
+
+            // Booking a ticket
+            tbm.BookTicket(1);
+
+            // Display available tickets after booking
+            tbm.DisplayAvailableTickets();
         }
     }
 }
